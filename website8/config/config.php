ab04eb2e62c751e988bfeaf155acda505465321b
hunterluker <hunterluker1992@gmail.com>
2019-06-18 11:11:18 -0600

    fetch data with mysqli

---
+++ website8/config/config.php
@@ -0,0 +1,6 @@
+<?php
+    define('ROOT_URL', 'http://localhost/phpsandbox/website8/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '00mangamanga3675');
+    define('DB_NAME', 'phpblog');
